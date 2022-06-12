6552aa04077229adca2ed36149eb1bf71d5d6832
xn4k <stueckholz@hotmail.com>
2022-06-12 19:44:03 +0200

    Added vue axios for the future work with the API requests

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+//Axios import
+//import VueAxios from "vue-axios";
+//import axios from "axios";
 createApp(App).use(router).mount('#app')
