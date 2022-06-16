e1ad1093adbcae9168714fc29c7aec4491f7e191
xn4k <stueckholz@hotmail.com>
2022-06-16 15:07:14 +0200

    Deleted old api test, added spotify api env and testet the access server

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,22 +18,5 @@ export default {
   }
 }
 
-const axios = require("axios");
 
-let api_key = '31af009d0emsh31086eda8f02b30p131b82jsn7ea985ba33de';
-const options = {
-  method: 'GET',
-  url: 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly',
-  params: {lat: '35.5', lon: '-78.5'},
-  headers: {
-    'X-RapidAPI-Key': api_key,
-    'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
-  }
-};
-
-axios.request(options).then(function (response) {
-  console.log(response.data);
-}).catch(function (error) {
-  console.error(error);
-});
 </script>
