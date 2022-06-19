61b3da6f8dbc28ebdbf11eb2b4e92013d6634366
xn4k <stueckholz@hotmail.com>
2022-06-19 18:17:43 +0200

    added api call with axios and let it print it out on the weather page

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -4,7 +4,10 @@
     <h1>hallloooooiiiiiiiii</h1>
     <p>Lets try to make an API call</p>
     <button class="apibutton">TESTBUTTON 4 the API</button>
-    <p></p>
+
+    <div id="app">
+      {{ info }}
+    </div>
 
 
 
@@ -12,9 +15,32 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "Weather"
+  el: '#app',
+  name: "Weather",
+  data() {
+    return {
+      info: null
+    };
+  },
+  mounted() {
+    axios
+        .get('https://api.weatherapi.com/v1/current.json?key=ffc4143944e6422ab0602017221806&q=Cologne')
+        .then(response => (this.info = response));
+  }
 }
+
+
+/*
+new Vue({
+  
+});
+*/
+
+
+/*
 //https://zetcode.com/javascript/axios/ axios stuff
 const axios = require('axios');
 
@@ -31,9 +57,11 @@ async function makeRequest() {
   console.log(res);
 }
 
-makeRequest();
+makeRequest();*/
 </script>
 
 <style scoped>
-
+#app {
+  padding-top: 55px;
+}
 </style>
