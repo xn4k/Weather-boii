007ba785090d455d75fbb279210191dfb84e489b
Mikhail Zhivoderov <Mikhail.Zhivoderov@sunzinet.com>
2022-07-28 17:07:43 +0200

    [EDIT] - created method , get weather and bind on button

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -3,14 +3,12 @@
   <div class="weather">
     <h1>hallloooooiiiiiiiii</h1>
     <p>Lets try to make an API call</p>
-    <button class="apibutton">TESTBUTTON 4 the API</button>
+    <p>You can choose your city first:</p>
+    <button @click="loadWeather" class="apibutton">TESTBUTTON 4 the API</button>
 
     <div id="app">
       {{ info }}
     </div>
-
-
-
   </div>
 </template>
 
@@ -25,14 +23,17 @@ export default {
       info: null
     };
   },
-  mounted() {
-    axios
-        .get('https://api.weatherapi.com/v1/current.json?key=ffc4143944e6422ab0602017221806&q=Cologne')
-        .then(response => (this.info = response))
-        .catch(error => console.log(error))
-        console.log(this.info);
-  }
+  methods: {
+    loadWeather() {
+      axios
+          .get('https://api.weatherapi.com/v1/current.json?key=ffc4143944e6422ab0602017221806&q=Cologne')
+          .then(response => (this.info = response))
+          .catch(error => console.log(error))
+      console.log(this.info);
+    }
+  },
 }
+
 
 
 /*
@@ -65,5 +66,14 @@ makeRequest();*/
 <style scoped>
 #app {
   padding-top: 55px;
+  background: aqua;
+}
+
+.weather {
+  background: #42b983;
+}
+
+.background {
+  background: #42b983;
 }
 </style>
